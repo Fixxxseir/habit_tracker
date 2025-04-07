698bdc6b7227fbae2bfa0446be9f495730faf0cc
mexic <mexic2284@mail.ru>
2025-04-08 01:58:51 +0300

    регистрация в админ панели модели пользователя

--- users/admin.py
+++ users/admin.py
@@ -5,5 +5,7 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    """
+    Регистрация модели в админ панели
+    """
     list_display = ("id", "email", "username")
-
