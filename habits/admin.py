9946ee7039df4acb5e4c9a2e1d321dd215b11ad7
mexic <mexic2284@mail.ru>
2025-04-08 01:58:37 +0300

    регистрация в админ панели модели привычки

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """
+    Регистрация модели в админ панели
+    """
+    list_display = (
+        "id",
+        "user",
+        "location",
+        "time",
+        "action",
+        "is_positive_habit",
+        "associated_habit",
+        "periodicity",
+        "reward",
+        "time_to_complete",
+        "is_published",
+    )
